42b91eb9c55d754b6ee07630fe94a038bc0ddd1b
mateusz-sobierajski <mateusz.sobierajski.official@gmail.com>
2023-01-29 17:32:48 +0100

    Nadanie stylu ekranowi logowania

--- screens/Login.js
+++ screens/Login.js
@@ -1,6 +1,9 @@
-import React, {useEffect, useState} from 'react'
-import {View, Text, TextInput, Button} from 'react-native'
+import React, { useEffect, useState } from 'react';
+import {View, Text, TextInput, Button} from 'react-native';
 import {db} from '../config'
+import MyButton from "../Elements/MyButton";
+import {loginStyles} from "../Styles/LoginStyles";
+import {styles} from "../Styles/styles";
 import {collection, query, where, getDocs} from 'firebase/firestore'
 
 const LoginScreen = ({navigation}) => {
@@ -47,58 +50,35 @@ const LoginScreen = ({navigation}) => {
 
     console.log(error)
 
-    return (<View style={styles.container}>
-        <Text style={styles.welcomeText}>Please login!</Text>
-        <View style={styles.formContainer}>
-            <Text style={styles.prompt}>Login:</Text>
-            <TextInput
-                style={styles.input}
-                placeholder='Login'
-                keyboardType='email-address'
-                value={email}
-                onChangeText={(oldEmail) => setEmail(oldEmail)}
-            />
-            <Text style={styles.prompt}>Password:</Text>
-            <TextInput
-                style={styles.input}
-                placeholder='Password'
-                secureTextEntry={true}
-                value={password}
-                onChangeText={(oldPassword) => setPassword(oldPassword)}
-            />
+
+    return (
+        <View style={loginStyles.login_container}>
+            <Text style={styles.welcomeText}>Please login!</Text>
+            <View style={loginStyles.formContainer}>
+                <Text style={styles.text}>Login:</Text>
+                <TextInput
+                    style={loginStyles.input}
+                    placeholder="Login"
+                    keyboardType="email-address"
+                />
+                <Text style={styles.text}>Password:</Text>
+                <TextInput
+                    style={loginStyles.input}
+                    placeholder="Password"
+                    secureTextEntry={true}
+                />
+            </View>
+
+            <MyButton onPress={handleLogin} style={styles.helloButton}>
+                <Text style={loginStyles.buttonText}>Login</Text>
+            </MyButton>
+
+            <MyButton onPress={() => {navigation.navigate('Register') }} style={styles.button}>
+                <Text style={loginStyles.buttonText}>Register</Text>
+            </MyButton>
+
         </View>
-        <Button style={styles.button} onPress={handleLogin} title={'Login'}>
-            <Text style={styles.buttonText}>Login</Text>
-        </Button>
-        <Button
-            style={styles.button}
-            onPress={() => {
-                navigation.navigate('SignUpScreen')
-            }}
-            title={'Register'}
-        >
-            <Text style={styles.buttonText}>Register</Text>
-        </Button>
-        {error && <Text style={styles.errorText}>{error}</Text>}
-    </View>)
-}
-const styles = {
-    login_container: {
-        flex: 1, alignItems: 'center', justifyContent: 'center',
-    }, welcomeText: {
-        fontSize: 34, marginBottom: 256,
-    }, formContainer: {
-        alignSelf: 'stretch', paddingHorizontal: 24,
-    }, prompt: {
-        marginTop: 24,
-    }, input: {
-        height: 48, borderWidth: 1, borderColor: '#ccc', paddingHorizontal: 8, marginBottom: 36,
-    }, button: {
-        backgroundColor: '#4da6ff', height: 48, alignItems: 'center', justifyContent: 'center', marginTop: 24,
-    }, buttonText: {
-        color: '#fff', fontWeight: 'bold',
-    }, errorText: {
-        textAlign: 'center', color: 'red',
-    },
-}
-export default LoginScreen
+    );
+};
+
+export default LoginScreen;
