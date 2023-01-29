ca148d6753496ce4e1649e96c4e55b61d3c2e022
mateusz-sobierajski <mateusz.sobierajski.official@gmail.com>
2023-01-29 16:39:17 +0100

    Wgranie pliku styles.js, ułożenie ekranu powitania

---
+++ screens/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import {styles} from "../styles";
+import MyButton from "../Elements/MyButton";
+
+const MainScreen = ({navigation}) => {
+    return (
+        <View style={styles.welcomeScreen}>
+            <Text style={styles.welcomeText}>Witamy W Kawiarni Kim-Chi!</Text>
+
+            <Image source={require('../images/welcome_img.jpg')} style={styles.welcomeImage} />
+
+            <MyButton onPress={() => {navigation.navigate('Login')}} style={styles.helloButton}> START </MyButton>
+            <MyButton onPress={() => {navigation.navigate('Settings') }} style={styles.button}> TO SETTINGZ </MyButton>
+
+        </View>
+    );
+}
+
+export default MainScreen;
