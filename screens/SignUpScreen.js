b4c47b04da584b6299c5f229a023e12b671f0044
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-02-15 18:44:04 +0100

    Chuj wie co dziala i czemu xD

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,20 +1,20 @@
 import React, {useState} from 'react';
 import {View, TextInput, Button} from 'react-native';
-import {db} from "../firebase";
 import * as authLib from "firebase/auth";
+import {db} from "../firebase";
 export default function SignupScreen() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSignup = () => {
-        const auth = authLib.getAuth();
-        console.log(auth);
+        const auth = authLib.getAuth(db.app);
         authLib.createUserWithEmailAndPassword(auth,email, password)
             .then((userCredential) => {
                 console.log(userCredential);
                 console.log('User account created successfully');
             })
             .catch((error) => {
+                console.log(auth, email, password)
                 console.log(error);
             });
     };
