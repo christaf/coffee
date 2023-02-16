1059b16531caf112195aa39eb27d7ad96d2428a4
mateusz-sobierajski <mateusz.sobierajski.official@gmail.com>
2023-02-16 18:22:10 +0100

    Tab icons

--- screens/MainTab.js
+++ screens/MainTab.js
@@ -1,5 +1,6 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import React from "react";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import favourite from "./MainTab/Favourite";
 import productMenu from "./MainTab/ProductMenu";
@@ -18,11 +19,41 @@ const MainTab = () => {
 
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Menu" component={ProductMenu}/>
-            <Tab.Screen name="Favourites" component={FavouriteScreen} />
-            <Tab.Screen name="Map" component={RestaurantMap} />
-            <Tab.Screen name="Cart" component={Cart} />
-            <Tab.Screen name="Settings" component={Settings} />
+            <Tab.Screen name="Menu" component={ProductMenu}
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <Icon name="silverware-fork-knife" color={color} size={size} />
+                            ),
+                        }}
+            />
+            <Tab.Screen name="Favourites" component={FavouriteScreen}
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <Icon name="heart-outline" color={color} size={size} />
+                            ),
+                        }}
+            />
+            <Tab.Screen name="Map" component={RestaurantMap}
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <Icon name="map-marker" color={color} size={size} />
+                            ),
+                        }}
+            />
+            <Tab.Screen name="Cart" component={Cart}
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <Icon name="cart-outline" color={color} size={size} />
+                            ),
+                        }}
+            />
+            <Tab.Screen name="Settings" component={Settings}
+                        options={{
+                            tabBarIcon: ({ color, size }) => (
+                                <Icon name="cog-outline" color={color} size={size} />
+                            ),
+                        }}
+            />
         </Tab.Navigator>
     );
 }
