159e4304c89adf776129cc3eef312e7c098c7677
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-02-19 01:09:47 +0100

    Firebase connection v4

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
-import firebase from "firebase/compat";
-
+import React, {useState} from 'react';
+import {View, TextInput, Button} from 'react-native';
+import * as authLib from "firebase/auth";
+import {db} from "../config";
 export default function SignupScreen() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSignup = () => {
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(() => {
+    const handleSignup = (navigation) => {
+        const auth = authLib.getAuth(db.app);
+        const auth2 = authLib.getAuth();
+
+        auth2.createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                console.log(userCredential.user.email, password);
                 console.log('User account created successfully');
+                alert("Successfully signed up")
+                navigation.navigate("Menu")
+            return userCredential.user.sendEmailVerification();
             })
             .catch((error) => {
+                console.log(email, password)
                 console.log(error);
             });
+
+
     };
 
     return (
