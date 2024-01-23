283af6893136634c97617213d75a663aa59cd8b6
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2024-01-23 11:13:05 +0100

    backend
    poprawiony backend,
    dodano endpointy do discountow itd

--- Backend/DatabaseHandling/fetch_favourites.py
+++ Backend/DatabaseHandling/fetch_favourites.py
@@ -2,6 +2,51 @@ from .utils import connect_to_database
 import json
 
 
+def update_favourites(favourite):
+    conn = connect_to_database()
+
+
+def insert_favourites(data):
+
+        conn = connect_to_database()
+        try:
+            cursor = conn.cursor()
+
+            query = """
+                INSERT INTO favourite_items (
+                    default_coffee,
+                    sweetner_type,
+                    sweetner_amount,
+                    creamer_type,
+                    creamer_amount,
+                    additive_1,
+                    additive_2,
+                    additive_3
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            """
+
+            for item in data:
+                values = (
+                    item['default_coffee'],
+                    item['sweetner_type'],
+                    item['sweetner_amount'],
+                    item['creamer_type'],
+                    item['creamer_amount'],
+                    item['additive_1'],
+                    item['additive_2'],
+                    item['additive_3']
+                )
+
+                cursor.execute(query, values)
+
+            conn.commit()
+            print("Data inserted successfully!")
+
+        finally:
+            conn.close()
+
+
+
 def fetch_favourites():
     conn = connect_to_database()
     cur = conn.cursor()
@@ -26,8 +71,8 @@ def fetch_favourites():
     for (
             favourite_item_id,
             default_coffee,
-            sweetener_type,
-            sweetener_amount,
+            sweetner_type,
+            sweetner_amount,
             creamer_type,
             creamer_amount,
             additive_1,
@@ -37,8 +82,8 @@ def fetch_favourites():
         item = {
             "favourite_item_id": favourite_item_id,
             "default_coffee": default_coffee,
-            "sweetener_type": sweetener_type,
-            "sweetener_amount": sweetener_amount,
+            "sweetner_type": sweetner_type,
+            "sweetner_amount": sweetner_amount,
             "creamer_type": creamer_type,
             "creamer_amount": creamer_amount,
             "additive_1": additive_1,
--- Backend/app.py
+++ Backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from DatabaseHandling.fetch_coffees import fetch_coffees
 from DatabaseHandling.check_login_handler import check_login
 from DatabaseHandling.check_and_register import insert_user
-from DatabaseHandling.fetch_favourites import fetch_favourites
+from DatabaseHandling.fetch_favourites import fetch_favourites, update_favourites, insert_favourites
 from DatabaseHandling.utils import read_config
 
 app = Flask(__name__)
@@ -52,6 +52,34 @@ def get_favourite_list():
     result = fetch_favourites()
 
     return jsonify(result)
+
+
+@app.route('/add_favourite', methods=['PUT'])
+def add_favourite(content):
+    content = [
+        {'favourite_item_id': 15, 'default_coffee': 25, 'sweetner_type': 'Sugar', 'sweetner_amount': 1,
+         'creamer_type': 'Milk', 'creamer_amount': 3, 'additive_1': 'Cinnamon', 'additive_2': 'Vanilla extract',
+         'additive_3': 'Whipped cream'},
+        {'favourite_item_id': 23, 'default_coffee': 24, 'sweetner_type': 'Honey', 'sweetner_amount': 2,
+         'creamer_type': 'Almond milk', 'creamer_amount': 3, 'additive_1': 'Caramel syrup', 'additive_2': 'Nutmeg',
+         'additive_3': 'None'},
+        {'favourite_item_id': 523, 'default_coffee': 26, 'sweetner_type': 'Stevia', 'sweetner_amount': 1,
+         'creamer_type': 'Soy milk', 'creamer_amount': 3, 'additive_1': 'Hazelnut syrup',
+         'additive_2': 'Chocolate powder', 'additive_3': 'Whipped cream'}
+    ]
+    # request.json
+    print(content)
+    result = insert_favourites(content)
+
+    return jsonify(result)
+
+
+discounts = ['10%', '20%', 'Free Coffee', '15%', 'No Discount', '25%']
+
+
+@app.route('/discounts', methods=['GET'])
+def get_discounts():
+    return jsonify(discounts)
 
 
 if __name__ == "__main__":
